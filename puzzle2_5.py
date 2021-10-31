2c71a799b5739d12bc917c96c6bdc8a31d4fd58a
judplaza <58659463+judplaza@users.noreply.github.com>
2021-10-31 21:02:12 +0100

    Update puzzle2_5.py

--- puzzle2_5.py
+++ puzzle2_5.py
@@ -6,12 +6,6 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GLib, GObject
 
 
-import gi
-
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-
-
 class MyWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="UID indentification")
